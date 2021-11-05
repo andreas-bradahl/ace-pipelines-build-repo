d7e2724699d1a84aa2414860ded6e9477e8b96c5
Andreas Bradahl <andreas.bradahl@avella.no>
2021-11-05 20:07:59 +0100

    replace filename with parameter

--- scripts/get_repo_list.py
+++ scripts/get_repo_list.py
@@ -8,7 +8,7 @@ def get_repo_names(response_dictionary):
     """Returns a list of repo names in the GH payload dictionary"""
     repos = [value['name'] for value in response_dictionary['items']]
 
-    print(f"\nFound {len(repos)} repositories:")
+    print(f"\n\nFound {len(repos)} repositories:")
     for repo in repos:
         print(repo)
 
@@ -27,7 +27,7 @@ def write_repos_to_file(repo_list, filename):
 
 def execute_github_request(user, topic):
     """Executes an http request to the GH API - finds repos which contains the parameter 'topic_name' in its topics"""
-    print(f"\nFind repos in org: '{user}', with topic: '{topic}'")
+    print(f"\n\nFind repos in org: '{user}', with topic: '{topic}'")
     url = f"https://api.github.com/search/repositories?q=user:{user}+topic:{topic}"
     # user = os.environ.get('GITHUB_USER')
     # token = os.environ.get('GITHUB_TOKEN')
@@ -39,18 +39,17 @@ def execute_github_request(user, topic):
 
 
 def main():
-    NUMBER_OF_ARGUMENTS = 3
+    NUMBER_OF_ARGUMENTS = 4
 
     if len(sys.argv) == NUMBER_OF_ARGUMENTS:
         user = sys.argv[1]
         topic = sys.argv[2]
+        filepath = sys.argv[3]
 
         response = execute_github_request(user, topic)
         repo_list = get_repo_names(response.json())
-
-        absolute_path = os.getcwd()
-        print(f'\nAbsolute path: {absolute_path}')
-        write_repos_to_file(repo_list, absolute_path + 'repos/repos.json')
+        
+        write_repos_to_file(repo_list, filepath)
     elif len(sys.argv) > NUMBER_OF_ARGUMENTS:
         print("Error: Too many arguments")
     else:
