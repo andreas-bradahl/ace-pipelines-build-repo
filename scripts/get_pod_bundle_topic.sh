ac127518242a81cc193400338e31b4ddc428ea5c
Andreas Bradahl <andreas.bradahl@avella.no>
2021-11-13 21:10:04 +0100

    Add comment to script

--- scripts/get_pod_bundle_topic.sh
+++ scripts/get_pod_bundle_topic.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+# Description:
+# Retrieves a GitHub topic that represents the pod which the application
+# belongs to. If it doesn't belong to a specific pod yet, null is
+# returned.
+#
+# Arguments:
+# $1: GitHub user/organization in which the repository resides
+# $2: GitHub repository to search for topic in
+#
+# @author: Andreas Bradahl <andreas.bradahl@tine.no>
+
 apk add jq curl > /dev/null
 
 curl \
