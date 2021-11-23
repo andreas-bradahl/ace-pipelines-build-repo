70b7aa370e176bed39eb8de5627b95f93651c4a2
Andreas Bradahl <andreas.bradahl@avella.no>
2021-11-23 13:26:09 +0100

    check if repos exist

--- scripts/fetch_dependencies_recursively.py
+++ scripts/fetch_dependencies_recursively.py
@@ -4,26 +4,30 @@ import json
 
 def fetch_deps_recursive(repo):
     url = f'https://github.com/tineikt/{repo}.git'
+    workspace = 'common-workspace'
 
     print('Currently in: ' + os.getcwd())
 
     os.chdir('/workspace')
 
-    clone = f'git clone -b cicd {url} common-workspace/{repo}'
-    os.system(clone)
-    os.chdir(f'common-workspace/{repo}')
-    
-    try:
-        with open('dependencies.json', 'r') as deps_file:
-            data = deps_file.read()
+    clone = f'git clone -b cicd {url} {workspace}/{repo}'
+    if not os.path.exists(f'{workspace}/{repo}'):
+        os.system(clone)
+        os.chdir(f'{workspace}/{repo}')
         
-        dependencies = json.loads(data)
-        for dep in dependencies:
-            print(dep)
-            if dep:
-                fetch_deps_recursive(dep)
-    except IOError:
-        print('No dependencies file for this repo.')
+        try:
+            with open('dependencies.json', 'r') as deps_file:
+                data = deps_file.read()
+            
+            dependencies = json.loads(data)
+            for dep in dependencies:
+                print(dep)
+                if dep:
+                    fetch_deps_recursive(dep)
+        except IOError:
+            print('No dependencies file for this repo.')
+    else:
+        print(f'Dependency {repo} already cloned.')
         
 def main():
     repo = sys.argv[1]
