d690124cd3d434935264237453d2e737e4c463dd
Andreas Bradahl <andreas.bradahl@avella.no>
2021-11-25 16:08:11 +0100

    parameterize repo workdir

--- scripts/fetch_dependencies_recursively.py
+++ scripts/fetch_dependencies_recursively.py
@@ -2,16 +2,15 @@ import sys
 import os
 import json
 
-def fetch_deps_recursive(repo, working_dir):
+def fetch_deps_recursive(repo, repos_working_dir):
     url = f'https://github.com/tineikt/{repo}.git'
-    workspace = 'common-workspace'
 
-    os.chdir(working_dir)
+    os.chdir(repos_working_dir)
 
-    clone = f'git clone -b cicd {url} {workspace}/{repo}'
-    if not os.path.exists(f'{workspace}/{repo}'):
+    clone = f'git clone -b cicd {url} {repos_working_dir}/{repo}'
+    if not os.path.exists(f'{repos_working_dir}/{repo}'):
         os.system(clone)
-        os.chdir(f'{workspace}/{repo}')
+        os.chdir(f'{repos_working_dir}/{repo}')
         
         try:
             with open('dependencies.json', 'r') as deps_file:
@@ -20,7 +19,7 @@ def fetch_deps_recursive(repo, working_dir):
             dependencies = json.loads(data)
             for dep in dependencies:
                 if dep:   
-                    fetch_deps_recursive(dep, working_dir)
+                    fetch_deps_recursive(dep, repos_working_dir)
         except IOError:
             print(f'No dependencies file for {repo}.')
     else:
@@ -28,9 +27,9 @@ def fetch_deps_recursive(repo, working_dir):
         
 def main():
     repo = sys.argv[1]
-    working_dir = sys.argv[2]
+    repos_working_dir = sys.argv[2]
 
-    fetch_deps_recursive(repo, working_dir)
+    fetch_deps_recursive(repo, repos_working_dir)
 
 if __name__ == "__main__":
     main()
