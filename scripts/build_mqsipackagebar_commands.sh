d614bf78704fac60a0f631ea3d177334cf8f77e5
Andreas Bradahl <andreas.bradahl@avella.no>
2021-11-29 10:31:23 +0100

    add test files and projects

--- scripts/build_mqsipackagebar_commands.sh
+++ scripts/build_mqsipackagebar_commands.sh
@@ -6,17 +6,17 @@ OUTFILE=$3
 
 set -eu
 
-jq -cr '.[]' $REPO_LIST | while read repo; do
-    TIP_NUMBER=${repo::6}
-    TIP_NUMBER=${TIP_NUMBER^^}
-    PACKAGE_COMMAND="mqsipackagebar -a ../bars/${TIP_NUMBER}.bar -k"
+jq -cr '.[]' "$REPO_LIST" | while read repo; do
+    TIP_NUMBER=${repo:3:3}
+    # TIP_NUMBER=${TIP_NUMBER^^}
+    PACKAGE_COMMAND="mqsipackagebar -a ../bars/${repo}.bar -k"
 
-    for dir in "$PROJECTS_WORKSPACE"/${TIP_NUMBER}*/; do
-        if [[ ${dir^^} == *"LIB/" || ${dir^^} == *"JAVA/" || $(basename ${dir^^}) == "TIP000"* ]]; then
+    for dir in "$PROJECTS_WORKSPACE"/[tT][iI][pP]${TIP_NUMBER}*/; do
+        if [[ ${dir^^} == *"LIB/" || ${dir^^} == *"JAVA/" || $(basename "${dir^^}") == "TIP000"* ]]; then
             continue
         else
             PACKAGE_COMMAND+=" "$(basename "$dir")
         fi
     done
-    echo $PACKAGE_COMMAND | tee -a $OUTFILE
+    echo "$PACKAGE_COMMAND" | tee -a "$OUTFILE"
 done
