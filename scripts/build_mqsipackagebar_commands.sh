e5e691f0e04c8bf6081be2cc54fec9be73a7e8a2
Andreas Bradahl <andreas.bradahl@avella.no>
2021-11-30 10:49:24 +0100

    add regex pattern

--- scripts/build_mqsipackagebar_commands.sh
+++ scripts/build_mqsipackagebar_commands.sh
@@ -10,8 +10,10 @@ jq -cr '.[]' "$REPO_LIST" | while read repo; do
     TIP_NUMBER=${repo:3:3}
     # TIP_NUMBER=${TIP_NUMBER^^}
     PACKAGE_COMMAND="mqsipackagebar -a ../bars/${repo}.bar -k"
+    # EXCLUDE_PATTERN='\b[tT][iI][pP]000[A-Za-z0-9-_]+|[tT][iI][pP]\d{3}[A-Za-z0-9-_]+[lL][iI][bB]|Java\b'
 
     for dir in "$PROJECTS_WORKSPACE"/[tT][iI][pP]${TIP_NUMBER}*/; do
+        # if [[ $(basename "$dir") =~ $EXCLUDE_PATTERN ]]; then
         if [[ ${dir^^} == *"LIB/" || ${dir^^} == *"JAVA/" || $(basename "${dir^^}") == "TIP000"* ]]; then
             continue
         else
