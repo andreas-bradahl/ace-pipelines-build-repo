9f34dae2af0a1d9378cf36432dce1232b793bb05
AndreasBradahl-Avella <andreas.bradahl@avella.no>
2021-12-17 15:14:13 +0100

    refactor

--- scripts/build_mqsipackagebar_commands.sh
+++ scripts/build_mqsipackagebar_commands.sh
@@ -9,15 +9,13 @@ set -eu
 jq -cr '.[]' "$REPO_LIST" | while read repo; do
     TIP_NUMBER=${repo:3:3}
     echo "Creating mqsipackagebar command for TIP$TIP_NUMBER"
-    # TIP_NUMBER=${TIP_NUMBER^^}
     PACKAGE_COMMAND="mqsipackagebar -a ../bars/${repo}.bar -k"
     # EXCLUDE_PATTERN='\b[tT][iI][pP]000[A-Za-z0-9-_]+|[tT][iI][pP]\d{3}[A-Za-z0-9-_]+[lL][iI][bB]|Java\b'
 
     for dir in "$PROJECTS_WORKSPACE"/*/; do
         echo "Checking folder $(basename $dir)"
-    # for dir in "$PROJECTS_WORKSPACE"/[tT][iI][pP]${TIP_NUMBER}*/; do
         # if [[ $(basename "$dir") =~ $EXCLUDE_PATTERN ]]; then
-        if [[ ${dir^^} == *"LIB/" || ${dir^^} == *"JAVA/" || $(basename "${dir^^}") == "TIP000"* ]]; then
+        if [[ $(basename "${dir^^}") == *"LIB" || $(basename "${dir^^}") == *"JAVA" || $(basename "${dir^^}") == "TIP000"* ]]; then
             echo "Folder $(basename $dir) matched pattern LIB, Java or TIP000 - skipping"
             continue
         elif [[ $(basename "$dir") =~ [tT][iI][pP]${TIP_NUMBER}* ]]; then
