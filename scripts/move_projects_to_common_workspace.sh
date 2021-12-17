0d7067ac300ab681b90a8609bb9672dee8f366fb
AndreasBradahl-Avella <andreas.bradahl@avella.no>
2021-12-17 14:52:28 +0100

    refactor

--- scripts/move_projects_to_common_workspace.sh
+++ scripts/move_projects_to_common_workspace.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 # Start this script from the workspace where all application
-# folders are placed.
+# folders are placed. Application folders contain config, 
+# projects etc.
 #
 # Moves all project subfolders that are located 
 # inside the 'projects' folder of an 
 # application to a common workspace.
 
-WORKSPACE_DIR=$1
-PROJECTS_DIR=$2
+ALL_PROJECTS_WORKSPACE=$1
+PROJECTS_FOLDER=$2
 
 # Create workspace directory
-if [[ ! -d "$WORKSPACE_DIR" ]]; then
-    mkdir -p $WORKSPACE_DIR
+if [[ ! -d "$ALL_PROJECTS_WORKSPACE" ]]; then
+    mkdir -p $ALL_PROJECTS_WORKSPACE
 fi
 
 for dir in */; do
     # Copy all directories in 'projects' folder to workspace
-    if [[ -d $dir/"$PROJECTS_DIR" ]]; then
-        echo "Putting $dir projects in projects_workspace"
-        cp -r $dir/$PROJECTS_DIR/* $WORKSPACE_DIR
+    if [[ -d $dir/"$PROJECTS_FOLDER" ]]; then
+        echo "Putting all $dir projects in $ALL_PROJECTS_WORKSPACE"
+        cp -r $dir/$PROJECTS_FOLDER/* $ALL_PROJECTS_WORKSPACE
     else
-        echo "No '$PROJECTS_DIR' folder found for application $dir."
+        echo "No '$PROJECTS_FOLDER' folder found for application $dir."
     fi
 done
