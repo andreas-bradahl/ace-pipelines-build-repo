a5108d7b9a3d79696ad1b90fd9aaec3b331c5dac
AndreasBradahl-Avella <andreas.bradahl@avella.no>
2022-01-22 10:03:37 +0100

    add script to replace labels

---
+++ scripts/replace_labels_in_integrationserver.py
@@ -0,0 +1,38 @@
+import sys
+
+from yaml import safe_load, dump
+
+commit_id = sys.argv[1]
+integration_server_file = sys.argv[2]
+event_repo = sys.argv[3]
+
+try:
+    with open(integration_server_file, 'r') as infile:
+        integration_server_object = safe_load(infile)
+except FileNotFoundError:
+    print('IntegrationServer manifest file not found - aborting ...')
+    sys.exit(1)
+except:
+    print('Error when reading IntegrationServer manifest file - aborting ...')
+    sys.exit(1)
+
+try:
+    integration_server_object['spec']['labels']['lastEventCommitId'] = commit_id
+except KeyError:
+    print('Could not find tag lastEventCommitId.')
+    print('Should be under spec.labels.lastEventCommitId - aborting ...')
+    sys.exit(1)
+
+try:
+    integration_server_object['spec']['labels']['lastEventRepo'] = event_repo
+except KeyError:
+    print('Could not find tag lastEventRepo.')
+    print('Should be under spec.labels.lastEventRepo - aborting ...')
+    sys.exit(1)    
+
+try:
+    with open(integration_server_file, 'w') as outfile:
+        dump(integration_server_object, outfile)
+except:
+    print('Error when writing to IntegrationServer manifest file - aborting ... ')
+    sys.exit(1)
